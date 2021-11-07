41c05874fb39c2d3c42859e387455911399cf2ec
Alina <freedomalina@mail.ru>
2021-11-07 12:23:38 +0500

    Added last function

--- TODO.js
+++ TODO.js
@@ -1,24 +1,39 @@
 const list = {
-     "do homework": "In Progress",
-     "cook a dinner": "Done",
-     "do yoga": "To Do",
+     "to do homework": "In Progress",
+     "to cook a dinner": "Done",
+     "to do yoga": "To Do",
+     "to learn english": "To Do",
+     "to make some girl stuff": "In Progress",
+     "to go shopping": "Done",
+     "to watch a move": "To Do",
+     "to have breakfast": "Done",
+     "to sleep": "To Do"
     }
-    function changeStatus(taskName, taskStatus){
+function changeStatus(taskName, taskStatus){
           list[taskName] = taskStatus;    
     }
 
-    function addTask(addNewTask) {
+function addTask(addNewTask) {
         list[addNewTask] = "To Do";              
     }
     
-    function deleteTask(taskToDelet){
+function deleteTask(taskToDelet){
         delete list[taskToDelet];    
     }
- /* function showList(){
-     
- }
+function showList(){
+       for (key in list){
+            if (list[key] === 'To Do'){
+                console.log(key);
 
-showList() */
+            } else if (list[key] === 'Done'){
+                 console.log(key);
+
+            }else if (list[key] === 'In Progress'){
+                 console.log(key);
+            }
+       }
+}    
+showList() 
 
 
 
@@ -27,7 +42,8 @@ deleteTask("cook a dinner");
 changeStatus("do homework", "Done");
 addTask("read the book");
 changeStatus("read the book", "In Progress");
-console.log (list)
+
+
 
 
 
