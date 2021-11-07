e07c209ba2907c87fed2f0229b606d6c926b76e1
Alina <freedomalina@mail.ru>
2021-11-07 16:13:04 +0500

    Added condition '-'

--- TODO.js
+++ TODO.js
@@ -22,39 +22,54 @@ function deleteTask(taskToDelet){
     }
 function showList(){
     console.log("TODO:");
+       let n=0;
         for (let key in list){
             if (list[key] === 'To Do'){
                 console.log(key);
-            }
+                  n++;
+            } 
         }
+            if (n === 0){
+                console.log('-');
+            }
+        
           
             
     console.log("Done:");
+        n = 0;
         for (let key in list){
-           if (list[key] === 'Done'){
+            if (list[key] === 'Done'){
                  console.log(key);
+                 n++;
             }
-        }
-         
+        }  
+            if(n === 0) {
+                 console.log('-');
+             }
    
     console.log("In Progress:"); 
+        n = 0;
         for (let key in list) {      
             if (list[key] === 'In Progress'){
                  console.log(key);
+                 n++;
+        } 
+            if(n === 0) {
+                console.log('-')
             }
-       }
-}  
-showList() 
-
-
-
-/*addTask("go for a walk");
+        }    
+                
+}
+       
+  
+ 
+addTask("go for a walk");
 deleteTask("cook a dinner");
 changeStatus("do homework", "Done");
 addTask("read the book");
-changeStatus("read the book", "In Progress");*/
+changeStatus("read the book", "In Progress");
 
-
+showList()
 
 
 
