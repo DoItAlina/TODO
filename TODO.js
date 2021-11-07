8049c8974869a0124ee3b1cb690561a37b4e8d47
Alina <freedomalina@mail.ru>
2021-11-07 19:00:19 +0500

    Added final version

--- TODO.js
+++ TODO.js
@@ -1,77 +1,51 @@
 const list = {
-     "to do homework": "In Progress",
-     "to cook a dinner": "Done",
-     "to do yoga": "To Do",
-     "to learn english": "To Do",
-     "to make some girl stuff": "In Progress",
-     "to go shopping": "Done",
-     "to watch a move": "To Do",
-     "to have breakfast": "Done",
-     "to sleep": "To Do"
-    }
+ "to do homework": "In Progress",
+ "to cook a dinner": "Done",
+ "to do yoga": "To Do",
+    "to learn english": "To Do",
+    "to make some girl stuff": "In Progress",
+    "to go shopping": "Done",
+    "to watch a move": "To Do",
+    "to have breakfast": "Done",
+    "to sleep": "To Do"
+}
 function changeStatus(taskName, taskStatus){
-          list[taskName] = taskStatus;    
-    }
+        list[taskName] = taskStatus;    
+}
 
 function addTask(addNewTask) {
-        list[addNewTask] = "To Do";              
-    }
-    
-function deleteTask(taskToDelet){
-        delete list[taskToDelet];    
-    }
-function showList(){
-    console.log("TODO:");
-       let n=0;
-        for (let key in list){
-            if (list[key] === 'To Do'){
-                console.log(key);
-                  n++;
-            } 
-        }
-            if (n === 0){
-                console.log('-');
-            }
-        
-          
-            
-    console.log("Done:");
-        n = 0;
-        for (let key in list){
-            if (list[key] === 'Done'){
-                 console.log(key);
-                 n++;
-            }
-        }  
-            if(n === 0) {
-                 console.log('-');
-             }
-   
-    console.log("In Progress:"); 
-        n = 0;
-        for (let key in list) {      
-            if (list[key] === 'In Progress'){
-                 console.log(key);
-                 n++;
-        } 
-            if(n === 0) {
-                console.log('-')
-            }
-        }    
-                
+    list[addNewTask] = "To Do";              
 }
-       
-  
- 
-addTask("go for a walk");
+
+function deleteTask(taskToDelet){
+    delete list[taskToDelet];    
+}
+
+function showStatus(status){
+console.log(status + ":");
+    let n=0;
+    for (let key in list){
+        if (list[key] === status){
+                console.log(key);
+                n++;
+        } 
+    }
+        if (n === 0){
+            console.log('-');
+        }              
+}
+    
+function showList(){
+    showStatus('In Progress')             
+    showStatus('To Do')
+    showStatus('Done')
+    
+ }
+
+/*addTask("go for a walk");
 deleteTask("cook a dinner");
 changeStatus("do homework", "Done");
 addTask("read the book");
-changeStatus("read the book", "In Progress");
+changeStatus("read the book", "In Progress");*/
 
 showList()
-
-
-
-  
-
